874748be7a96275a169ee1d81840533f941cdc0b
JonsAngEng <LiamJonson@rambler.ru>
2019-09-05 20:36:47 +0300

    eew

---
+++ testiii.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+import math
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+
+#browser = webdriver.Chrome()
+## говорим WebDriver искать каждый элемент в течение 5 секунд
+#
+#browser.get("http://suninjuly.github.io/explicit_wait2.html")
+##pr1 = EC.text_to_be_present_in_element((By.ID, 'price'), '100')
+#pr = WebDriverWait(browser, 12).until(EC.text_to_be_present_in_element((By.ID, "price"), "100"))
+#
+#button = browser.find_element_by_tag_name("button").click()
+#
+#num = browser.find_element_by_id("input_value").text
+#num = int(num)
+#print(num)
+#num2 = abs(12 * math.sin(int(num)))
+#print(num2)
+#browser = browser.find_element_by_id("answer").send_keys(str(num2))
+#time.sleep(5)
+#button6 = browser.find_element_by_id("solve")
+#button6.click()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
