5b1335e07407fb1756fea62a0f4f6f9ddd8d4801
JonsAngEng <LiamJonson@rambler.ru>
2019-09-06 22:59:47 +0300

    eew'

--- testiii.py
+++ testiii.py
@@ -6,41 +6,16 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 
-#browser = webdriver.Chrome()
-## говорим WebDriver искать каждый элемент в течение 5 секунд
-#
-#browser.get("http://suninjuly.github.io/explicit_wait2.html")
-##pr1 = EC.text_to_be_present_in_element((By.ID, 'price'), '100')
-#pr = WebDriverWait(browser, 12).until(EC.text_to_be_present_in_element((By.ID, "price"), "100"))
-#
-#button = browser.find_element_by_tag_name("button").click()
-#
-#num = browser.find_element_by_id("input_value").text
-#num = int(num)
-#print(num)
-#num2 = abs(12 * math.sin(int(num)))
-#print(num2)
-#browser = browser.find_element_by_id("answer").send_keys(str(num2))
-#time.sleep(5)
-#button6 = browser.find_element_by_id("solve")
-#button6.click()
+
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+browser = webdriver.Chrome()
+browser.get("http://suninjuly.github.io/redirect_accept.html")
+button = browser.find_element_by_tag_name("button").click()
+new_window = browser.window_handles[1]
+browser.switch_to.window(new_window)
+num = browser.find_element_by_id("input_value").text
+browser.find_element_by_id("answer").send_keys(calc(int(num)))
+browser.find_element_by_tag_name("button").click()
