8b600043fc7a8d56749fe690a6fdde1dd7215ce4
JonsAngEng <LiamJonson@rambler.ru>
2019-09-17 22:07:27 +0300

    test_abs_project.py

--- testiii.py
+++ testiii.py
@@ -1,21 +1,6 @@
-from selenium import webdriver
-import math
-import time
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium import webdriver
+def test_abs1():
+    assert abs(-42) == 42, "Should be absolute value of a number"
 
-
-def calc(x):
-    return str(math.log(abs(12 * math.sin(int(x)))))
-
-
-browser = webdriver.Chrome()
-browser.get("http://suninjuly.github.io/redirect_accept.html")
-button = browser.find_element_by_tag_name("button").click()
-new_window = browser.window_handles[1]
-browser.switch_to.window(new_window)
-num = browser.find_element_by_id("input_value").text
-browser.find_element_by_id("answer").send_keys(calc(int(num)))
-browser.find_element_by_tag_name("button").click()
+if __name__ == "__main__":
+    test_abs1()
+    print("All tests passed!")
