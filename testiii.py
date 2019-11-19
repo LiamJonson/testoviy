ad9b84d5fd5fdb0bee44912a6d9162113612f3d4
LiamJonson <liamJonson@rambler.ru>
2019-11-19 11:18:37 +0300

    Write a program that takes an integer as input, which outputs True if the transmitted value falls into the set of combined intervals {−10} ∪ (−5,3] ∪ (8,12) ∪ [16, + ∞) and False otherwise .

--- testiii.py
+++ testiii.py
@@ -1,28 +1,4 @@
-import pytest
-from selenium import webdriver
-import time
-import math
-
-@pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
-
-@pytest.mark.parametrize('link',['https://stepik.org/lesson/236895/step/1',
-        'https://stepik.org/lesson/236896/step/1','https://stepik.org/lesson/236897/step/1','https://stepik.org/lesson/236898/step/1','https://stepik.org/lesson/236899/step/1',
-         'https://stepik.org/lesson/236903/step/1','https://stepik.org/lesson/236904/step/1','https://stepik.org/lesson/236905/step/1'])
-def test_guest(browser, link):
-    browser.get(link)
-    time.sleep(3)
-    browser.find_element_by_tag_name('textarea').send_keys(str(math.log(int(time.time()))))
-    browser.find_element_by_tag_name("button").click()
-    time.sleep(3)
-    assert 'Correct!' == browser.find_element_by_class_name('smart-hints__hint').text
-
-
-
+n = int(input())
+print(n == -10 or (-5 < n <= 3) or (8 < n < 12) or (16 <= n))
 
 
