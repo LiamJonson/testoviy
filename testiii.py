ff33736dbd0390c905c01c744ea0d7f2f1fad229
LiamJonson <Liamjonson@rambler.ru>
2019-12-25 16:09:43 +0300

    Input format: The first line contains two natural numbers 1 \ le n, m \ le 1001≤n, m≤100, after which nn lines contain a description of the field as a sequence of points '.' and asterisks '*', where a dot means an empty cell, and an asterisk - a cell with a mine. Output format: nn lines of the field, in each cell of which there will be either a number from 0 to 8, or a mine (indicated by the symbol "*"), and the number means the number of mines in neighboring cells of the field.

--- testiii.py
+++ testiii.py
@@ -1,7 +1,14 @@
-n = input()
-rom_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-konv_num = [rom_num[i] for i in n]
-for i in range(len(konv_num) - 1):
-    if konv_num[i] < konv_num[i + 1]:
-        konv_num[i] = konv_num[i] * (-1)
-print(sum(konv_num))
+n, m = map(int, input().split())
+p = [[i for i in input()] for _ in range(n)]
+k = [[0 for i in range(m)] for j in range(n)]
+for i in range(0, n):
+    for j in range(0, m):
+        for di in range(-1, 2):
+            for dj in range(-1, 2):
+                if i + di < n and i + di > -1 and j + dj < m and j + dj > -1:
+                    if p[i + di][j + dj] == '*' and p[i][j] != '*':
+                        k[i][j] += 1
+                    elif p[i][j] == '*':
+                        k[i][j] = '*'
+for i in k:
+    print(*i, sep='')
