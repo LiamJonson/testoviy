a8165afac436f0968cdf9baf43bef0072e281dea
LiamJonson <Liamjonson@rambler.ru>
2019-12-28 19:55:30 +0300

    Input format: Two integers separated by spaces. Output format: On a separate line, each number from the segment or the word that replaces it.

--- testiii.py
+++ testiii.py
@@ -1,1 +1,10 @@
-print(int(input())**int(input()))
+n, m = map(int, input().split())
+for i in range(n, m + 1):
+    if i % 3 == 0 and i % 5 == 0:
+        print('FizzBuzz')
+    elif i % 3 == 0 and i % 5 != 0:
+        print('Fizz')
+    elif i % 3 != 0 and i % 5 == 0:
+        print('Buzz')
+    else:
+        print(i)
