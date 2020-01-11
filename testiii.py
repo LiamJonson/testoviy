fb4c494ad34d6c723e6d81a1e71cde6be0cfedd5
LiamJonson <Liamjonson@rambler.ru>
2020-01-11 13:15:04 +0300

    Input format: Two words, each on a separate line. A word can consist only of Latin characters of arbitrary register. The case of characters should not affect the response.
    Output format:
    True or False.

--- testiii.py
+++ testiii.py
@@ -1,16 +1,6 @@
-def get_int(start_message, error_message, end_message):
-    print(start_message)
-    while True:
-        p = input()
-        try:
-            int(p)
-        except:
-            print(error_message)
-        else:
-            print(end_message)
-            return int(p)
-
-
-
-
-x = get_int('Input int number:', 'Wrong value. Input int number:', 'Thank you.')
+a = input().lower()
+b = input().lower()
+if sorted(a) == sorted(b):
+    print('True')
+else:
+    print('False')
