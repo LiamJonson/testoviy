a68482a0572a354b9f8d7a8ad6bd9a1fc810a403
LiamJonson <Liamjonson@rambler.ru>
2020-01-12 14:43:59 +0300

    Input format: The first line contains two integers nn and mm, the number of rows and columns, respectively. This is followed by nn lines containing mm integers each, separated by a space.
    Output format:
    The program should print mm lines of contents of the transposed matrix. Matrix elements should be separated by a space.

--- testiii.py
+++ testiii.py
@@ -1,6 +1,8 @@
-a = input().lower()
-b = input().lower()
-if sorted(a) == sorted(b):
-    print('True')
-else:
-    print('False')
+a,b = map(int, input().split())
+n = list(zip(*[input().split() for i in range(a)]))
+for i in n:
+    print(*i)
+
+
+
+
