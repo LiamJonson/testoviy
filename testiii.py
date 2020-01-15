e95716d1a66c679c91c8e43b2a3a37583d8144a9
LiamJonson <Liamjonson@rambler.ru>
2020-01-15 15:02:09 +0300

    Format vvoda: Stroka, soderzhashchaya obyknovennuyu drob' v formate chislitel'/znamenatel'.
    Format vyvoda:
    Stroka s posledovatel'nost'yu koeffitsiyentov, zapisannykh cherez probel.
    Развернуть
    169/5000
    Input format:
    A string containing an ordinary fraction in the numerator / denominator format.
    
    Output format:
    A string with a sequence of coefficients written with a space.

--- testiii.py
+++ testiii.py
@@ -1,8 +1,8 @@
-a,b = map(int, input().split())
-n = list(zip(*[input().split() for i in range(a)]))
-for i in n:
-    print(*i)
-
-
-
-
+a,b = map(int, input().split('/'))
+k =[]
+while b != 0:
+    l = b
+    a,b =divmod(a,b)
+    k.append(a)
+    a = l
+print(*k)
