ecd5884339761db51318e3f986fef8e405b3630d
LiamJonson <Liamjonson@rambler.ru>
2020-01-19 16:06:08 +0300

    Input format: One line with a phrase like this: <number> <unit_from> in <unit_to> for example, if the phrase "15.5 mile in km" has come, then you need to translate 15.5 miles into kilometers.
    Output format:
    Fractional number in scientific format (exponential), with exactly two decimal places.

--- testiii.py
+++ testiii.py
@@ -1,8 +1,9 @@
-a,b = map(int, input().split('/'))
-k =[]
-while b != 0:
-    l = b
-    a,b =divmod(a,b)
-    k.append(a)
-    a = l
-print(*k)
+units = {'mile': 1609, 'yard': 0.9144, 'foot': 0.3048, 'inch': 0.0254, 'km': 1000, 'm': 1.0, 'cm': 0.01, 'mm': 0.001}
+v = input().split(' ')
+print(v)
+k = (float(v[0]) * units[v[1]] / units[v[3]])
+print(k)
+print('{:.4E}'.format(k))
+#print('%.2e'%(k))
+#print(a)
+
