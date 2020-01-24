c7b04250e2fd521514dad5bd79f62c94e575620d
LiamJonson <Liamjonson@rambler.ru>
2020-01-24 17:13:10 +0300

    Input format: A string containing a natural number nn, 0 <n <4000.
    Output format:
    A string containing a number encoded in the Roman numeral system.

--- testiii.py
+++ testiii.py
@@ -1,9 +1,33 @@
-units = {'mile': 1609, 'yard': 0.9144, 'foot': 0.3048, 'inch': 0.0254, 'km': 1000, 'm': 1.0, 'cm': 0.01, 'mm': 0.001}
-v = input().split(' ')
-print(v)
-k = (float(v[0]) * units[v[1]] / units[v[3]])
-print(k)
-print('{:.4E}'.format(k))
-#print('%.2e'%(k))
-#print(a)
+n = input()
+sl = {1: 'I', 4: 'IV', 5: 'V', 9: 'IX', 10: 'X', 40: 'XL', 50: 'L', 90: 'XC', 100: 'C', 400: 'CD', 500: 'D', 900: 'CM',
+      1000: 'M'}
+k = []
+def ost(x):
+    er = 0
+    h= x
+    for j in sl.keys():
+        if h in sl:
+            print(sl[h], end='')
+            break
+        elif h >j and h > 1000:
+            print(sl[1000]*int(str(h)[0]),end='')
+            break
+        elif h > j:
+            er = j
+        else:
+            print(sl[er], end='')
+            ost(h - er)
+            break
+p = 0
+for i in n:
+    k.append(int(i + '0' * (len(range(p, len(n) - 1)))))
+    p+=1
+for i in k:
+    if i in sl:
+        print(sl[i], end='')
+    elif i == 0:
+        continue
+    else:
+        ost(i)
+
 
