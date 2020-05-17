e2d497c5dd27f8d3f9201866e6861207cef54840
Germes <liamjonson@rambler.ru>
2020-05-17 13:28:42 +0300

    апишите функцию mean, которая будет считать среднее арифметическое переданных в нее аргументов.

--- testiii.py
+++ testiii.py
@@ -1,13 +1,9 @@
-import xlrd
+def mean(*numbers):
+    n = 0 
+    for i in numbers:
+        print(i)
+        n+=i
+    return float(n/len(numbers))
 
-rb = xlrd.open_workbook('trekking1.xlsx')
-sheet = rb.sheet_by_index(0)
 
-vals = [sheet.row_values(rownum) for rownum in range(sheet.nrows)]
-sub = vals[0][1]
-result = {}
-for i in vals[1:]:
-    result[i[0]] = i[1]
-k = sorted(result.items(), key=lambda x: (-x[1], x[0]))
-for j,i in enumerate(k):
-    print(i[0])
+print(mean(1,2,2,4))
