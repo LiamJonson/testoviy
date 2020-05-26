41a55197488401083981988614a44069880ba440
Germes <liamjonson@rambler.ru>
2020-05-26 21:40:57 +0300

    The chief accountant of Horns and Hooves accidentally deleted a payroll sheet. Fortunately, he retained the payroll of all employees. Help restore payroll from these payroll sheets. An archive with payment sheets is available at https://stepik.org/media/attachments/lesson/245299/rogaikopyta.zip (you can download and unzip it manually or learn how to do it yourself using a Python script).
    The statement must contain 1000 lines, each line should indicate the name of the employee and, through a space, his salary. Employees must be sorted alphabetically.

--- testiii.py
+++ testiii.py
@@ -1,30 +1,25 @@
 import xlrd
+import io
+import zipfile
+import requests
+r = requests.get("https://stepik.org/media/attachments/lesson/245299/rogaikopyta.zip")
+with r, zipfile.ZipFile(io.BytesIO(r.content)) as archive:
+    archive.extractall()
+x = 1
+xy = []
+while x !=1001:
+    rb = xlrd.open_workbook(str(x) + '.xlsx')
+    sheet = rb.sheet_by_index(0)
+    vals = [sheet.row_values(rownum) for rownum in range(sheet.nrows)]
+    g = [vals[1][1], int(vals[1][3])]
+    xy.append(g)
+    #print(vals[1][1], int(vals[1][3]))
+    x += 1
+for i in sorted(xy):
+    print(*i)
 
-rb = xlrd.open_workbook('trekking2.xlsx')
-sheet = rb.sheet_by_index(1)
-sheet1 = rb.sheet_by_index(0)
 
-vals = [sheet.row_values(rownum) for rownum in range(sheet.nrows)]
-vals1 = [sheet1.row_values(rownum) for rownum in range(sheet1.nrows)]
-kall = 0
-gProt = 0
-gFats = 0
-gCarb = 0
-k = []
-for i in vals1[1:]:
-    for j in vals[1:]:
-        if i[0] == j[0]:
-            k.append(i)
-            k[-1].append(j[1])
-for i in k:
 
-    kall += float(i[1]) / 100 * float(i[5])
-    gProt += float(i[2]) / 100 * float(i[5])
-    gFats += float(i[3]) / 100 * float(i[5])
-    if type(i[4]) == str:
-        gCarb += 0
-    else:
-        gCarb += float(i[4]) / 100 * float(i[5])
-print(int(kall), int(gProt), int(gFats), int(gCarb))
+
 
 
