a7334fa243b8702c1ae883d606f29973eff157a6
LiamJonson <liamjonson@rambler.ru>
2020-11-20 17:39:11 +0300

    У разведчиков должна быть хорошая память. Поэтому, когда их берут на работу им устраивают специальную проверку. Показывают ненадолго несколько разных чисел, а потом кандидат должен их назвать. Все равно в каком порядке он их вспоминает, и повторяется он или нет, но он должен назвать все числа. Напишите программу, которая принимает на вход две строки с числами. В первой строке показанные кандидату, а во второй ответы кандидата. В результате выведите "YES", если кандидат прошел испытание и его можно брать на работу, и "NO" в противном случае.

--- testiii.py
+++ testiii.py
@@ -1,27 +1,6 @@
-﻿def kaprekar(n, base=10):
-    def convert(num, to_base=10, from_base=10):
-        n = int(str(num), base=from_base)
-        c = ''
-        alphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        while n:
-            c = alphabet[n % to_base] + c
-            n //= to_base
-        return c
-    h = int(str(n),base=base)
-    k = h ** 2
-    g =convert(k,base)
-    for i in range(1,len(str(g))):
-        a,b = int(str(g[:i]),base),int(str(g[i:]),base)
-        if a*b and a+b == h:
-            return True
-    return False
-
-test_1 = [9, 45, 55, '99', '297', 703, 999, '2223', 2728, '4879']
-test_2 = [10, 46, 56, 100, 298, 704, '1000', '2224', '2729', '4880']
-test_3 = ['6', 'A', 'F', '33', '55', '5B', '78', '88', 'AB', 'CD', 'FF', '15F', '334', '38E']
-
-print([kaprekar(i) for i in test_1]) # Тест чисел Капрекара из системы с основанием 10
-
-print([kaprekar(i) for i in test_2 ]) # Тест НЕ чисел Капрекара из системы с основанием 10
-
-print([kaprekar(i, base=16) for i in test_3]) #Т
+﻿x = '8 11 12 23'
+y = '23 12 11 11 23 12 8'
+if set(x.split()) ==  set(y.split()):
+    print('YES')
+else:
+    print("NO")
