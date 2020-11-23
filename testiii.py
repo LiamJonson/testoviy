7f863342867eb1f0cd197fa026d611043bea45e9
LiamJonson <liamjonson@rambler.ru>
2020-11-23 14:30:16 +0300

    Напишите программу, которая прочитает количество детей, имена, которые вспомнила мать, имена, которые вспомнил отец. На выходе программа должна вывести количество детей, чьи имена родители не помнят.

--- testiii.py
+++ testiii.py
@@ -1,5 +1,5 @@
-﻿
-x = 'Norma, Anja'
-y = 'Alexandra, Roma, Jettie, Phung, Daron'
-print(*[i for i in set(y.split(', ')).union(x.split(', '))],sep=', ')
-#print(*x+y,sep =', ')
+﻿i = int(input())
+x = 'Anja, Tom, Alexandra, Roma'
+y = 'Phung'
+#x, y = input(),input()
+print(int(input() - len([i for i in set(input().split(', ')).union(input().split(', '))]))
