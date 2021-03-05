596f2d8cafbbadbfe7e8b13f976c3ba978294b44
Liamjonson <liamjonson@rambler.ru>
2021-03-05 12:37:01 +0300

    Implémentez la fonction de cryptage que le pirate de Kidd de l'histoire d'Edgar Allan sur Golden Beetle.

--- testiii.py
+++ testiii.py
@@ -1,5 +1,17 @@
-﻿i = int(input())
-x = 'Anja, Tom, Alexandra, Roma'
-y = 'Phung'
-#x, y = input(),input()
-print(int(input() - len([i for i in set(input().split(', ')).union(input().split(', '))]))
+﻿def kidds_encryption(text, reverse=False):
+    sl = {'e': '8', 't': ';', 'h': '4', 'o': '‡', 's': ')', 'n': '*', 'a': '5', 'i': '6', 'r': '(',
+          'f': '1', 'd': '†', 'l': '0', 'm': '9', 'b': '2', 'y': ':', 'g': '3', 'u': '?', 'v': '¶', 'c': '-', 'p': '.'}
+
+    text = text.lower().replace(',', '').replace(' ', '').replace('\'', '').replace('x', '')
+    k = [i for i in text]
+    dec = ''
+    if reverse == True:
+        ty = {j:i for i,j in sl.items()}
+        for i in k:
+            dec += ty[i]
+    for i in k:
+        if i in sl:
+            dec += sl[i]
+    return dec
+
+
