57f6715de270bd9f6ffdd3844106839bf03387ea
Liamjonson <liamjonson@rambler.ru>
2021-03-11 14:18:19 +0300

    Implement Enigma (Text, Ref, Rot1, Rot2, Rot3), where: Text - source text that needs to encrypt Ref - Reflector Number Rot1, Rot2, Rot3 - Rotor Numbers The function returns encrypted text.

--- testiii.py
+++ testiii.py
@@ -1,24 +1,35 @@
-﻿def rotor(symbol, n, reverse=False):
+﻿def enigma(text, ref, rot1, rot2, rot3):
     ROTORS = {0: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
               1: 'EKMFLGDQVZNTOWYHXUSPAIBRCJ',
               2: 'AJDKSIRUXBLHWTMCQGZNPYFVOE',
-              3: 'BDFHJLCPRTXVZNYEIWGAKMUSQO',
-              4: 'ESOVPZJAYQUIRHXLNFTGKDCMWB',
-              5: 'VZBRGITYUPSDNHLXAWMJQOFECK',
-              6: 'JPGVOUMFYQBENHZRDKASXLICTW',
-              7: 'NZJHGRCXMYSWBOUFAIVLPEKQDT',
-              8: 'FKQHTLXOCBJSPDZRAMEWNIUYGV',
-              'beta': 'LEYJVCNIXWPBQMDRTAKZGFUHOS',
-              'gamma': 'FSOKANUERHMBTIYCWLQPZXVGJD'
-              }
-    if reverse ==True:
-        return ROTORS[0][ROTORS[n].index(symbol)]
+              3: 'BDFHJLCPRTXVZNYEIWGAKMUSQO'}
+    REFLECTORS = {0: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+                  1: 'YRUHQSLDPXNGOKMIEBFZCWVJAT',
+                  2: 'FVPJIAOYEDRZXWGCTKUQSBNMHL',
+                  3: 'ENKQAUYWJICOPBLMDXZVFTHRGS',
+                  4: 'RDOBJNTKVEHMLFCWZAXGYIPSUQ',
+                  }
+    ui=''
+    text = text.upper().replace(',', '').replace(' ', '')
+    for i in text:
+        k=i
+        for i1 in [rot3, rot2, rot1]:
+            print(i)
+            k = ROTORS[i1][ROTORS[0].index(k)]
+        s = REFLECTORS[ref][REFLECTORS[0].index(k)]
+        for i2 in [rot1, rot2, rot3]:
+            s = ROTORS[0][ROTORS[i2].index(s)]
 
-    return ROTORS[n][ROTORS[0].index(symbol)]
+        ui+=s
+
+    return ui
 
 
 
 
-for i in 'EKMFLGDQVZNTOWYHXUSPAIBRCJ':
-    print((rotor(i, 1,reverse=True)),end='')
+
+
+
+
+print((enigma('Some encripted text', 1, 1, 2, 3)),end='')
 
